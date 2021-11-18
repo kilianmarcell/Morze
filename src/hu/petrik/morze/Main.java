2439589abb5520c72cc39215a6cde411a1f2e0b5
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-11-18 11:18:20 +0100

    MorzeABC beolvasva

--- src/hu/petrik/morze/Main.java
+++ src/hu/petrik/morze/Main.java
@@ -1,7 +1,29 @@
 package hu.petrik.morze;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        List<String> morzeKod = Files.readAllLines(Paths.get("morze.txt"));
+        List<String> morzeAbc = Files.readAllLines(Paths.get("morzeabc.txt"));
+
+        String[] betuk = new String[morzeAbc.size()];
+        String[] kod = new String[morzeAbc.size()];
+
+        for (int i = 1; i < morzeAbc.size(); i++) {
+
+            String[] egysor = morzeAbc.get(i).split("\t");
+            betuk[i] = egysor[0];
+            kod[i] = egysor[1];
+
+        }
+
     }
 }
